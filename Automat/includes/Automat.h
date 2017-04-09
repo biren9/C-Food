55ca4de07bf28d7b7909410748309e428e7020cc
klarmeister <philipp.r.c.bott@gmail.com>
2017-04-09 23:57:21 +0200

    Funktionen des Automaten im Header definitert.

--- Automat/includes/Automat.h
+++ Automat/includes/Automat.h
@@ -8,10 +8,45 @@
 #ifndef Automat_H_
 #define Automat_H_
 
+#include "../C-Food/Buffer/includes/Buffer.h"
+#include "../C-Food/Scanner/src/String.h"
+
 class Automat {
 public:
-	Automat();
+	Automat(Buffer);
 	virtual ~Automat();
+
+    enum state {
+        startState , identifier, identifierType, number,
+        equalSign, equalColon, colonSign, commentStart,
+        commentEnd, andSign, endState
+    };
+
+    void nextToken();
+    String getTokenLiteral();
+    //tokenType getCurrentTokenType();
+    int getStartLine();
+    int getStartColumn();
+    state getCurrentState();
+
+
+private:
+    unsigned int startLine, startColumn;
+    String tokenLiteral;
+    //tokenType currentTokenType;
+    state currentState;
+
+    void startTransient();
+    void identifierTransient();
+    void identifierTypeTransient();
+    void numberTransient();
+    void equalSignTransient();
+    void equalColonTransient();
+    void colonSignTransient();
+    void commentStartTransient();
+    void commentEndTransiend();
+    void andSignTransient();
+
 };
 
 #endif /* Automat_H_ */
